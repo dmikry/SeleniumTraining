6b2b14357aeaf6c14c16c58dcaff309d31498d65
Dmytro K <carlos.dk87@gmail.com>
2023-05-13 14:48:00 +0300

    Add project files.

---
+++ FirstTest.cs
@@ -0,0 +1,36 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumTraining
+
+{
+	public class Tests
+	{
+		IWebDriver driver;
+
+		[SetUp]
+		public void Setup()
+		{
+			driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+		}
+
+		
+		[Test]
+		public void Test1()
+		{
+			driver.Navigate().GoToUrl("https://google.com");
+			driver.Manage().Window.Maximize();
+			driver.FindElement(By.CssSelector("[name=q]")).SendKeys("JLL" + "\n");
+			
+
+
+		}
+
+		[TearDown]
+
+		public void TearDown()
+		{
+		//	driver.Quit();
+		}
+	}
+}
