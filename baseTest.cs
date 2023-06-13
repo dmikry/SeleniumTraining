ffd0c64218a50a2df840039df9548ac679d2f1bc
dmikry <carlos.dk87@gmail.com>
2023-06-13 16:47:27 +0300

    Basic flow. Need to fix

---
+++ baseTest.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+
+namespace SeleniumTraining;
+
+[TestFixture]
+public class BaseTest
+{
+	[OneTimeSetUp]
+	public void Setup()
+	{
+		drv = new ChromeDriver();
+	}
+
+	[OneTimeTearDown]
+	public void ShutDown()
+	{
+		drv.Quit();
+	}
+
+	protected IWebDriver drv;
+
+	protected internal static string DemoHtmlUrl()
+	{
+		return "file://" + Path.Combine(Environment.CurrentDirectory, @"Resources\", "Examples.html");
+	}
+}
