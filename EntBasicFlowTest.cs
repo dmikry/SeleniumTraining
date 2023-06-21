4dca53c7bd3c26e7ff5e8086abf0b4685d6dac20
dmikry <carlos.dk87@gmail.com>
2023-06-21 16:11:40 +0300

    test merge

--- EntBasicFlowTest.cs
+++ EntBasicFlowTest.cs
@@ -6,11 +6,15 @@ using OpenQA.Selenium.Chrome;
 namespace SeleniumTraining;
 
 [TestFixture]
-public class EntBasicFlowTest : BaseTest
+public class EntBasicFlowTest
 {
+	private IWebDriver drv;
 	[Test]
 	public void LoginTest()
+
 	{
+		drv = new ChromeDriver();
+		var pickUpComment = "Pick-Up Comment From Autotest";
 		drv.Navigate().GoToUrl(Environment.GetEnvironmentVariable("ENT_QA_URL"));
 		drv.Manage().Window.Maximize();
 		drv.FindElement(By.Name("username")).SendKeys(Environment.GetEnvironmentVariable("ENT_QA_USER"));
@@ -22,23 +26,52 @@ public class EntBasicFlowTest : BaseTest
 
 		Thread.Sleep(4000);
 
-		var version = drv.FindElement(By.CssSelector(".menu-logo img")).GetAttribute("title");
-
-		
+			
 		drv.FindElement(By.CssSelector(".menu-primary > .menu-list > li:nth-of-type(2) > .menu-drop")).Click();
 		drv.FindElement(By.CssSelector("[aria-owns='dropdown-894']")).Click();
 
+		Thread.Sleep(2000);
+		// Preserve WO# with New Status that is going to be Picked Up to return to t later
+		var woLink = By.XPath("//td[@data-column='WOStatus'][contains(text(), 'New')][1]/following-sibling::td/a");
+		var woNumber = drv.FindElement(woLink).Text;
 
-		drv.FindElement(By.XPath("(//td[text()='New'])[1]")).Click();
-		drv.FindElement(By.CssSelector("span[title='Pick-Up']")).Click();
-		drv.FindElement(By.CssSelector(".id-btn-save")).Click();
-		drv.FindElement(By.CssSelector(".id-view-details")).Click();
-		drv.FindElement(By.CssSelector("div#WoActivityLogGrid  .kg-collapse > div")).Click();
+		//Click on New WO and open Quick View dialog
+		drv.FindElement(woLink).Click();
+		Thread.Sleep(2000);
 
-		var record = drv.FindElement(By.CssSelector(".menu-logo img")).GetAttribute("title");
+		// Action Pick Up
+		drv.FindElement(By.XPath("//div[@data-role='woqvdialog']//span[@title='Pick-Up']")).Click();
+		Thread.Sleep(2000);
 
-		Assert.That(record, Is.EqualTo("Picked Up"));
+		//Activate Comment area
+		drv.FindElement(By.XPath("//form[@class='corrigo-form']/div/textarea")).Click();
+		Thread.Sleep(2000);
 
+		// Type a Comment
+		drv.FindElement(By.CssSelector("form.corrigo-form textarea")).SendKeys(pickUpComment);
+		Thread.Sleep(2000);
+
+		//Close WO Quick View dialog
+		drv.FindElement(By.CssSelector("div[data-role=woactionpickupeditdialog] button.id-btn-save")).Click();
+		Thread.Sleep(2000);
+
+		//Check in the Activity log that Action "Picked Up" is displayed
+		var action = drv.FindElement(By.XPath("//*[@data-role='woactivityloggrid']//tbody//tr[1]//td[@data-column='ActionTitle']"));
+		Assert.That(action.Text, Is.EqualTo("Picked Up"));
+
+		//Check in the Activity log that Comment is displayed
+		var comment =
+			drv.FindElement(By.XPath("//*[@data-role='woactivityloggrid']//tbody//tr[1]//td[@data-column='Comment']"));
+		Assert.That(comment.Text, Is.EqualTo(pickUpComment));
+
+		//Closing the Quick View dialog
+		drv.FindElement(By.XPath("//button[@class='close btn-dismiss']")).Click();
+		Thread.Sleep(1000);
+
+		//Check WO Staus is Opened on WO list page
+		//var woStatus = drv.FindElement(By.XPath("//td[@data-column='Number']/a[contains(text(), '" + woNumber + "')]/../../td[@data-column='WOStatus']"));
+		//Thread.Sleep(1000);
+		//Assert.That(woStatus.Text, Is.EqualTo("Open"));
 
 	}
 }
